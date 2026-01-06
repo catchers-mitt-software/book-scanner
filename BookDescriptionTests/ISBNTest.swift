99caa5e133cb99dc20047c60c9a879cec144480a
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-05 23:40:31 -0500

    New test class

---
+++ BookDescriptionTests/ISBNTest.swift
@@ -0,0 +1,16 @@
+//
+//  ISBNTest.swift
+//  BookScannerTests
+//
+//  Created by Alonso del Arte on 1/5/26.
+//
+
+import Testing
+
+struct ISBNTest {
+
+    @Test func testPlaceholder() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
