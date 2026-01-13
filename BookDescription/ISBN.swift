d797fc7a5e5622e0965ccd2968d2079299749a45
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-13 18:25:09 -0500

    Made CustomStringConvertible (feels very inelegant)

--- BookDescription/ISBN.swift
+++ BookDescription/ISBN.swift
@@ -5,27 +5,36 @@
 //  Created by Alonso del Arte on 12/19/25.
 //
 
-struct ISBN {
+struct ISBN : CustomStringConvertible {
     
     let digits: UInt64
     
     let checkDigit: UInt8
     
+    var displayForm: String
+    
+    var description: String {
+        self.displayForm
+    }
+    
     // TODO: Write tests for this
     init(_ number: UInt64) {
         self.digits = 0
         self.checkDigit = 0
+        self.displayForm = "0"
     }
     
     // TODO: Write tests for this
     init(_ number: String) {
         self.digits = 0
         self.checkDigit = 0
+        self.displayForm = "0"
     }
     
     private init(_ digits: UInt64, _ checkDigit: UInt8) {
         self.digits = 0
         self.checkDigit = 0
+        self.displayForm = "0"
     }
     
 }
