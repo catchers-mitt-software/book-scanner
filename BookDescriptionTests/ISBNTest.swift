92f5ea49f65d1f0e91cd15cbfa62911417b943e3
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-13 18:53:10 -0500

    Jotted down ISBN-10 to ISBN-13 example

--- BookDescriptionTests/ISBNTest.swift
+++ BookDescriptionTests/ISBNTest.swift
@@ -8,6 +8,15 @@
 import Testing
 
 struct ISBNTest {
+    
+    // TODO: Test conversion from ISBN-10 to ISBN-13
+    // For example, Donald Knuth's The Art of Computer Programming, Volume I, as
+    // published by Addison-Wesley in 1997, has ISBN-10 number 0201896834
+    // (that's how the Wayne State University Library System website shows it to
+    // me). According to https://www.isbn.org/ISBN_converter that converts to
+    // ISBN-13 978-0-201-89683-1. Notice the prefix 978 is added, and all the
+    // original digits are retained, except check digit 4 is discarded and the
+    // check digit is recalculated as 1.
 
     @Test func testPlaceholder() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
