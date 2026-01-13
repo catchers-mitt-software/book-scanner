f9f9cc6927284a496d0495a487fac191c2ebaeee
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-13 16:25:03 -0500

    Starting to flesh out with constructors

--- BookDescription/ISBN.swift
+++ BookDescription/ISBN.swift
@@ -5,4 +5,12 @@
 //  Created by Alonso del Arte on 12/19/25.
 //
 
-struct ISBN {}
+struct ISBN {
+    
+    // TODO: Write tests for this
+    init(_ number: Int64) {}
+    
+    // TODO: Write tests for this
+    init(_ number: String) {}
+    
+}
