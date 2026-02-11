92b0eee43ee77e1864d0facf5cc4c949cb3cdf88
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-11 17:17:51 -0500

    Failing remove dashes test when there are none to remove

--- BookDescriptionTests/ISBNTest.swift
+++ BookDescriptionTests/ISBNTest.swift
@@ -9,6 +9,14 @@ import Testing
 
 struct ISBNTest {
     
+    @Test func testRemoveDashesWhenThereAreNoneToRemove() {
+        let expected: UInt64 = 9780000000000 +
+                UInt64.random(in: 0 ... 10000000000)
+        let numStr: String = expected.description
+        let actual: UInt64 = ISBN.removeDashes(numStr)
+        #expect(actual == expected)
+    }
+    
     // TODO: Test conversion from ISBN-10 to ISBN-13
     // For example, Donald Knuth's The Art of Computer Programming, Volume I, as
     // published by Addison-Wesley in 1997, has ISBN-10 number 0201896834
@@ -17,9 +25,5 @@ struct ISBNTest {
     // ISBN-13 978-0-201-89683-1. Notice the prefix 978 is added, and all the
     // original digits are retained, except check digit 4 is discarded and the
     // check digit is recalculated as 1.
-
-    @Test func testPlaceholder() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
 
 }
