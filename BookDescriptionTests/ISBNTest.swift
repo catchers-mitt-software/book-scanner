3b7714c54c74093dfc7393b329af2af820521e05
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-13 23:25:06 -0500

    Also failing tests for check digit 6

--- BookDescriptionTests/ISBNTest.swift
+++ BookDescriptionTests/ISBNTest.swift
@@ -159,6 +159,22 @@ struct ISBNTest {
         #expect(actual == expected, message)
     }
     
+    @Test func testReckonISBN13CheckDigitSixForPrefix978() {
+        let expected: UInt8 = 6
+        let digits = ISBNTest.chooseNumber(978, expected)
+        let actual = ISBN.reckonISBN13CheckDigit(digits)
+        let message: Comment = "\(digits) should have check digit \(expected)"
+        #expect(actual == expected, message)
+    }
+    
+    @Test func testReckonISBN13CheckDigitSixForPrefix979() {
+        let expected: UInt8 = 6
+        let digits = ISBNTest.chooseNumber(979, expected)
+        let actual = ISBN.reckonISBN13CheckDigit(digits)
+        let message: Comment = "\(digits) should have check digit \(expected)"
+        #expect(actual == expected, message)
+    }
+    
     // TODO: Test conversion from ISBN-10 to ISBN-13
     // For example, Donald Knuth's The Art of Computer Programming, Volume I, as
     // published by Addison-Wesley in 1997, has ISBN-10 number 0201896834
