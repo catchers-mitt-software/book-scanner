43cdcd5fdf9909ec70431fac0d1d0a4944cc0109
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-13 23:39:11 -0500

    Passing all tests for reckonISBN13CheckDigit( )

--- BookDescription/ISBN.swift
+++ BookDescription/ISBN.swift
@@ -29,7 +29,24 @@ struct ISBN : CustomStringConvertible {
     // ISBN-10 numbers to ISBN-13. When an API responds with an ISBN-13 number,
     // it will be presumed valid.
     static func reckonISBN13CheckDigit(_ digits: UInt64) -> UInt8 {
-        12
+        var multiplier: UInt64 = 3
+        var curr: UInt64 = digits
+        var sum: UInt64 = 0
+        while (curr > 0) {
+            let digit = curr % 10
+            sum += digit * multiplier
+            curr /= 10
+            if (multiplier == 3) {
+                multiplier = 1
+            } else {
+                multiplier = 3
+            }
+        }
+        let modSum = sum % 10
+        if (modSum == 0) {
+            return 0
+        }
+        return UInt8(10 - modSum)
     }
 
     // TODO: Write tests for this
