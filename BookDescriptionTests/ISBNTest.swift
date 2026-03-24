2cdde02c3cb8c430496b204df17aaeb90a92aff7
Alonso del Arte <alonso.delarte@gmail.com>
2026-03-24 15:44:39 -0400

    Removed To Do comment that no longer applies

--- BookDescriptionTests/ISBNTest.swift
+++ BookDescriptionTests/ISBNTest.swift
@@ -372,15 +372,6 @@ struct ISBNTest {
         #expect(isbnA != isbnB)
     }
     
-    // TODO: Test conversion from ISBN-10 to ISBN-13
-    // For example, Donald Knuth's The Art of Computer Programming, Volume I, as
-    // published by Addison-Wesley in 1997, has ISBN-10 number 0201896834
-    // (that's how the Wayne State University Library System website shows it to
-    // me). According to https://www.isbn.org/ISBN_converter that converts to
-    // ISBN-13 978-0-201-89683-1. Notice the prefix 978 is added, and all the
-    // original digits are retained, except check digit 4 is discarded and the
-    // check digit is recalculated as 1.
-    
     /// Test that the constructor that takes a number converts a 10-digit number
     /// to ISBN-13. We will not worry whether the ISBN-10 number has a valid
     /// check digit or not.
