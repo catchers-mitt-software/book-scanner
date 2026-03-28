8da115c13efedc07b97b18c69699eb64d9cf0b68
Alonso del Arte <alonso.delarte@gmail.com>
2026-03-27 21:21:29 -0400

    New error class

---
+++ BookDescription/CheckDigitError.swift
@@ -0,0 +1,16 @@
+//
+//  CheckDigitError.swift
+//  BookScanner
+//
+//  Created by Alonso del Arte on 3/27/26.
+//
+
+import Foundation
+
+struct CheckDigitError : Error {
+    
+    init(woCheck: UInt64, erroneousCheck: UInt8) {
+        //
+    }
+    
+}
