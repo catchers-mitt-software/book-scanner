ae1f021cb36a100b1669801cb898a9fc04e7c2d1
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-03 22:21:04 -0400

    New test class

---
+++ BookDescriptionTests/CheckDigitErrorTest.swift
@@ -0,0 +1,16 @@
+//
+//  CheckDigitErrorTest.swift
+//  BookScannerTests
+//
+//  Created by Alonso del Arte on 4/3/26.
+//
+
+import Testing
+
+struct CheckDigitErrorTest {
+
+    @Test func testWoCheck() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
