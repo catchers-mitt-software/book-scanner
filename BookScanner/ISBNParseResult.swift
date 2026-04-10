e020bcb96d93e7601b759ac27505631d782313c7
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-10 00:22:48 -0400

    New class

---
+++ BookScanner/ISBNParseResult.swift
@@ -0,0 +1,28 @@
+//
+//  ISBNParseResult.swift
+//  BookScanner
+//
+//  Created by Alonso del Arte on 4/7/26.
+//
+
+import Foundation
+
+struct ISBNParseResult {
+    
+    let isbn: ISBN?
+    
+    let problem: CheckDigitError?
+    
+    // TODO: Write tests for this
+    init(num: UInt64) {
+        self.isbn = ISBN(0)
+        self.problem = nil
+    }
+    
+    // TODO: Write tests for this
+    init(s: String) {
+        self.isbn = ISBN(0)
+        self.problem = nil
+    }
+    
+}
---
+++ BookScannerTests/ISBNParseResultTest.swift
@@ -0,0 +1,16 @@
+//
+//  ISBNParseResultTest.swift
+//  BookScannerTests
+//
+//  Created by Alonso del Arte on 4/10/26.
+//
+
+import Testing
+
+struct ISBNParseResultTest {
+
+    @Test func placeholder() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
