30be35a92c29190e5dc4e70f471c647fedaf6b2d
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-13 10:30:12 -0400

    Failing ISBN-13 test for numeric constructor

--- BookScannerTests/ISBNParseResultTest.swift
+++ BookScannerTests/ISBNParseResultTest.swift
@@ -8,9 +8,33 @@
 import Testing
 
 struct ISBNParseResultTest {
+    
+    private static func choosePrefix() -> UInt64 {
+        if Bool.random() {
+            978
+        } else {
+            979
+        }
+    }
+    
+    private static func chooseAssignedDigits() -> UInt64 {
+        let prefix = choosePrefix()
+        let minimum = prefix * 1000000000
+        let maximum = minimum + 999999000
+        return UInt64.random(in: minimum ... maximum)
+    }
+    
+    private static func chooseISBN13Digits() -> UInt64 {
+        let num = chooseAssignedDigits()
+        let check = ISBN.reckonISBN13CheckDigit(num)
+        return 10 * num + UInt64(check)
+    }
 
-    @Test func placeholder() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test func testNumericConstructorISBN13() {
+        let number = ISBNParseResultTest.chooseISBN13Digits()
+        let expected = ISBN(number)
+        let actual = ISBNParseResult(number).isbn
+        #expect(actual == expected)
     }
 
 }
