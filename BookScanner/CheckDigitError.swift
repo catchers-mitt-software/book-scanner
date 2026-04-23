a3717a64af78228a6764c7f045553baba6063e71
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-22 20:58:09 -0400

    Little more for constructor stub

--- BookScanner/CheckDigitError.swift
+++ BookScanner/CheckDigitError.swift
@@ -9,8 +9,14 @@ import Foundation
 
 struct CheckDigitError : ParseError {
     
+    let unchecked: UInt64
+    
+    let badCheck: UInt8
+    
+    // TODO: Write tests for this
     init(woCheck: UInt64, erroneousCheck: UInt8) {
-        //
+        self.unchecked = 9800000000000
+        self.badCheck = 12
     }
     
 }
