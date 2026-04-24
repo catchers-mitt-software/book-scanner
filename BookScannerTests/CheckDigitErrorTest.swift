d3bb056217c25ebac4cb38a839f2b3aec2d38e4e
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-23 23:46:35 -0400

    Failing test of unchecked

--- BookScannerTests/CheckDigitErrorTest.swift
+++ BookScannerTests/CheckDigitErrorTest.swift
@@ -8,9 +8,24 @@
 import Testing
 
 struct CheckDigitErrorTest {
+    
+    private static func chooseDigitOtherThan(_ digit: UInt8) -> UInt8 {
+        var propDigit = digit
+        while (propDigit == digit) {
+            propDigit = UInt8.random(in: 0 ... 9)
+        }
+        return propDigit
+    }
 
-    @Test func testWoCheck() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test func testWoCheck() {
+        let correctCheckDigit = UInt8.random(in: 0 ... 9)
+        let prefix = ISBNTest.choosePrefix()
+        let expected = ISBNTest.chooseNumber(prefix, correctCheckDigit)
+        let badCheck = CheckDigitErrorTest
+            .chooseDigitOtherThan(correctCheckDigit)
+        let instance = CheckDigitError(expected, erroneousCheck: badCheck)
+        let actual = instance.unchecked
+        #expect(actual == expected)
     }
 
 }
