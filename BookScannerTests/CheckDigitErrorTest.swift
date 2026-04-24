0d9151131e9db6b521ac0124592ed2432a543a35
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-24 01:10:08 -0400

    Failing bad check test

--- BookScannerTests/CheckDigitErrorTest.swift
+++ BookScannerTests/CheckDigitErrorTest.swift
@@ -27,5 +27,17 @@ struct CheckDigitErrorTest {
         let actual = instance.unchecked
         #expect(actual == expected)
     }
+    
+    @Test func testBadCheck() {
+        let correctCheckDigit = UInt8.random(in: 0 ... 9)
+        let prefix = ISBNTest.choosePrefix()
+        let woCheck = ISBNTest.chooseNumber(prefix, correctCheckDigit)
+        let expected = CheckDigitErrorTest
+            .chooseDigitOtherThan(correctCheckDigit)
+        let instance = CheckDigitError(woCheck, erroneousCheck: expected)
+        let actual = instance.badCheck
+        let message: Comment = "For \(woCheck)-\(correctCheckDigit)"
+        #expect(actual == expected, message)
+    }
 
 }
