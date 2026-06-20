88f2024e2435bed82dabf31a5286bf2c16f2c024
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-20 16:16:08 -0400

    Put in Open Library API URL

--- BookScanner/BookService.swift
+++ BookScanner/BookService.swift
@@ -14,7 +14,8 @@ class BookService : ObservableObject {
     @Published var books: [Book] = []
     
     func getBook(isbn: ISBN) async throws {
-        let url = URL(string: "https://bookapi.com")
+        let url = URL(string:
+                        "https://openlibrary.org/isbn/\(isbn.description)")
         let (data, _) = try await URLSession.shared.data(from: url!)
         let decoder = JSONDecoder()
         let book = try decoder.decode(Book.self, from: data)
